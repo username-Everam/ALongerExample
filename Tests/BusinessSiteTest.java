ca1153597a6344f7e56461f7f569c308687d9b4f
username-Everam <simsimi.armand@gmail.com>
2025-02-13 15:18:10 +0100

    feat: add all tests with charge() 100% coverage

---
+++ Tests/BusinessSiteTest.java
@@ -0,0 +1,92 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BusinessSiteTest {
+
+    BusinessSite _subject;
+
+    @SuppressWarnings("deprecation")
+    @BeforeEach
+    public void setUp() {
+        new Zone ("A", 0.06, 0.07, new Date("15 May 2023"), new Date ("10 Sep 2023")).register();
+        new Zone ("B", 0.07, 0.06, new Date ("5 Jun 2023"), new Date ("31 Aug 2023")).register();
+        new Zone ("C", 0.065, 0.065, new Date ("5 Jun 2023"), new Date ("31 Aug 2023")).register();
+        _subject = new BusinessSite();
+    }
+
+    @SuppressWarnings("deprecation")
+    @Test
+    public void testZero() {
+        _subject.addReading(new Reading (10, new Date ("1 Jan 2023")));
+        _subject.addReading(new Reading (10, new Date ("1 Feb 2023")));
+        assertEquals(0d, _subject.charge().amount());
+    }
+
+    @SuppressWarnings("deprecation")
+    @Test
+    public void test100() {
+        _subject.addReading(new Reading (10, new Date ("1 Jan 2023")));
+        _subject.addReading(new Reading (110, new Date ("1 Feb 2023")));
+        assertEquals(7.27d, _subject.charge().amount());
+    }
+
+    @SuppressWarnings("deprecation")
+    @Test
+    public void test99() {
+        _subject.addReading(new Reading (100, new Date ("1 Jan 2023")));
+        _subject.addReading(new Reading (199, new Date ("1 Feb 2023")));
+        assertEquals(7.19d, _subject.charge().amount());
+    }
+
+    @SuppressWarnings("deprecation")
+    @Test
+    public void test101() {
+        _subject.addReading(new Reading (1000, new Date ("1 Jan 2023")));
+        _subject.addReading(new Reading (1101, new Date ("1 Feb 2023")));
+        assertEquals(7.34d, _subject.charge().amount());
+    }
+
+    @SuppressWarnings("deprecation")
+    @Test
+    public void test199() {
+        _subject.addReading(new Reading (10000, new Date ("1 Jan 2023")));
+        _subject.addReading(new Reading (10199, new Date ("1 Feb 2023")));
+        assertEquals(14.4d, _subject.charge().amount());
+    }
+
+    @SuppressWarnings("deprecation")
+    @Test
+    public void test200() {
+        _subject.addReading(new Reading (0, new Date ("1 Jan 2023")));
+        _subject.addReading(new Reading (200, new Date ("1 Feb 2023")));
+        assertEquals(14.48d, _subject.charge().amount());
+    }
+
+    @SuppressWarnings("deprecation")
+    @Test
+    public void test201() {
+        _subject.addReading(new Reading (50, new Date ("1 Jan 2023")));
+        _subject.addReading(new Reading (251, new Date ("1 Feb 2023")));
+        assertEquals(14.55d, _subject.charge().amount());
+    }
+
+    @SuppressWarnings("deprecation")
+    @Test
+    public void testMax() {
+        _subject.addReading(new Reading (0, new Date ("1 Jan 2023")));
+        _subject.addReading(new Reading (Integer.MAX_VALUE, new Date ("1 Feb 2023")));
+        assertEquals (1.5220290473E8, _subject.charge().amount());
+    }
+
+    @Test
+    public void testNoReadings() {
+        try {
+            _subject.charge();
+            assert(false);
+        } catch (NullPointerException e) {}
+    }
+}
--- Tests/ResidentialSiteTest.java
+++ Tests/ResidentialSiteTest.java
@@ -1,14 +1,82 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ResidentialSiteTest {
 
-    @Test
-    void addReading() {
+    ResidentialSite _subject;
+    @SuppressWarnings("deprecation")
+    Date dateDebut = new Date("21 Jun 2023");
+    @SuppressWarnings("deprecation")
+    Date dateFin = new Date ("21 Sep 2023");
+    @SuppressWarnings("deprecation")
+    Date dateAvant = new Date("21 Jan 2023");
+    @SuppressWarnings("deprecation")
+    Date dateAvant2 = new Date("21 May 2023");
+    @SuppressWarnings("deprecation")
+    Date dateApres = new Date("21 Oct 2023");
+    @SuppressWarnings("deprecation")
+    Date dateApres2 = new Date("21 Nov 2023");
+    @SuppressWarnings("deprecation")
+    Date datePendant = new Date("21 Jul 2023");
+    @SuppressWarnings("deprecation")
+    Date datePendant2 = new Date("21 Aug 2023");
+
+    @BeforeEach
+    public void setUp() {
+        Zone zone = new Zone ("A", 0.06, 0.07, dateDebut, dateFin).register();
+        _subject = new ResidentialSite(zone);
     }
 
     @Test
-    void charge() {
+    public void testZero() {
+        _subject.addReading(new Reading (10, dateAvant));
+        _subject.addReading(new Reading (10, dateAvant2));
+        assertEquals(0d, _subject.charge().amount());
+    }
+
+    @Test
+    public void test1() {
+        _subject.addReading(new Reading (10, dateApres));
+        _subject.addReading(new Reading (10, dateApres2));
+        assertEquals(0d, _subject.charge().amount());
+    }
+
+    @Test
+    public void test2() {
+        _subject.addReading(new Reading (10, dateAvant));
+        _subject.addReading(new Reading (10, datePendant));
+        assertEquals(0d, _subject.charge().amount());
+    }
+
+    @Test
+    public void test3() {
+        _subject.addReading(new Reading (10, datePendant));
+        _subject.addReading(new Reading (10, dateFin));
+        assertEquals(0d, _subject.charge().amount());
+    }
+
+    @Test
+    public void test4() {
+        _subject.addReading(new Reading (10, datePendant));
+        _subject.addReading(new Reading (10, datePendant2));
+        assertEquals(0d, _subject.charge().amount());
+    }
+
+    @Test
+    public void test5() {
+        _subject.addReading(new Reading (10, dateAvant));
+        _subject.addReading(new Reading (10, dateApres));
+        assertEquals(0d, _subject.charge().amount());
+    }
+
+    @Test
+    public void testMax() {
+        _subject.addReading(new Reading (0, dateDebut));
+        _subject.addReading(new Reading (Integer.MAX_VALUE, dateFin));
+        assertEquals (1.7475148177E8, _subject.charge().amount());
     }
 }
