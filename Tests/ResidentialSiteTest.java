ce132028d34c8d781e2f1d8f5d0ac71c49765e97
username-Everam <simsimi.armand@gmail.com>
2025-02-13 14:26:53 +0100

    feat: add tests customers

---
+++ Tests/ResidentialSiteTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResidentialSiteTest {
+
+    @Test
+    void addReading() {
+    }
+
+    @Test
+    void charge() {
+    }
+}
